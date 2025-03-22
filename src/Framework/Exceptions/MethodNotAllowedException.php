f6ff04aed8aec7fafbc9b965935cfa376897a6ba
JDW <xs@jdw.sx>
2025-03-22 19:29:54 -0400

    Rename MethodNotAllowed: MethodNotAllowedException

--- src/Framework/Exceptions/MethodNotAllowedException.php
+++ src/Framework/Exceptions/MethodNotAllowedException.php
@@ -7,7 +7,7 @@ declare( strict_types = 1 );
 namespace JDWX\Web\Framework\Exceptions;
 
 
-class MethodNotAllowed extends HttpStatusException {
+class MethodNotAllowedException extends HttpStatusException {
 
 
     public function __construct( string $i_stMessage = '', ?string $i_nstDisplay = null, ?\Throwable $i_ePrevious = null ) {

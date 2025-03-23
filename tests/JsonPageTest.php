a1688b33b02c1ff85b7747178add83ce0d72711e
JDW <xs@jdw.sx>
2025-03-23 01:06:15 -0400

    Fix tests.

--- tests/JsonPageTest.php
+++ tests/JsonPageTest.php
@@ -5,21 +5,23 @@ declare( strict_types = 1 );
 
 
 use JDWX\Web\JsonPage;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
 
 
-#[\PHPUnit\Framework\Attributes\CoversClass(JsonPage::class)]
-final class JsonPageTest extends \PHPUnit\Framework\TestCase {
+#[CoversClass(JsonPage::class)]
+final class JsonPageTest extends TestCase {
 
 
     public function testRender() : void {
         $page = new JsonPage( 'TEST_CONTENT' );
-        self::assertSame( '"TEST_CONTENT"', $page->render() );
+        self::assertSame( "\"TEST_CONTENT\"\n", $page->render() );
 
         $page = new JsonPage( [ 'foo' => 'bar' ] );
-        self::assertSame( '{"foo":"bar"}', $page->render() );
+        self::assertSame( "{\"foo\":\"bar\"}\n", $page->render() );
 
         $page = new JsonPage( [ 'foo' => 'bar' ], true );
-        self::assertSame( "{\n    \"foo\": \"bar\"\n}", $page->render() );
+        self::assertSame( "{\n    \"foo\": \"bar\"\n}\n", $page->render() );
     }
 
 }

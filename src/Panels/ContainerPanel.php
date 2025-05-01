cf047b67449609e96ddc9530fe19841a1e69927c
JDW <xs@jdw.sx>
2025-04-30 22:05:12 -0400

    Add missing PhpDoc for ContainerPanel constructor.

--- src/Panels/ContainerPanel.php
+++ src/Panels/ContainerPanel.php
@@ -10,6 +10,7 @@ namespace JDWX\Web\Panels;
 class ContainerPanel implements PanelInterface {
 
 
+    /** @param iterable<PanelInterface>|null $i_panels */
     public function __construct( ?iterable $i_panels = null ) {
         if ( is_iterable( $i_panels ) ) {
             $this->setPanels( $i_panels );

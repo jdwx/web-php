be1ca05a1c8d0e07c9197c48907f6c5958482b60
JDW <xs@jdw.sx>
2025-05-15 22:32:52 -0400

    Missed one.

--- src/Panels/YieldTrait.php
+++ src/Panels/YieldTrait.php
@@ -7,12 +7,15 @@ declare( strict_types = 1 );
 namespace JDWX\Web\Panels;
 
 
+use Stringable;
+
+
 trait YieldTrait {
 
 
     /**
-     * @param string|iterable<string> $i_chunk
-     * @return iterable<string>
+     * @param string|iterable<string|Stringable>|Stringable $i_chunk
+     * @return iterable<string|Stringable>
      */
     protected function yield( string|iterable $i_chunk ) : iterable {
         if ( is_string( $i_chunk ) ) {

045b1b3767bd23660c807e1937c5a90ba6e22ab7
JDW <xs@jdw.sx>
2026-02-14 16:30:52 -0500

    Update tests.

--- tests/SessionControlTest.php
+++ tests/SessionControlTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection UnnecessaryAssertionInspection */
 
 
 declare( strict_types = 1 );
@@ -17,12 +17,39 @@ use PHPUnit\Framework\TestCase;
 final class SessionControlTest extends TestCase {
 
 
+    public function testGet() : void {
+        self::assertInstanceOf( SessionControl::class, SessionControl::get() );
+
+        $x = new SessionControl();
+        SessionControl::set( $x );
+        self::assertSame( $x, SessionControl::get() );
+
+    }
+
+
     public function testNamespace() : void {
         $be = new MockSessionBackend( [ 'foo' => 'bar', 'baz' => [ 'foo' => 'qux' ] ] );
         $be->start();
         $sc = new SessionControl( $be );
         $ns = $sc->namespace();
         self::assertSame( 'bar', $ns->get( 'foo' ) );
+    }
+
+
+    public function testSetForBackend() : void {
+        $x = new MockSessionBackend( [] );
+        SessionControl::set( $x, 12345 );
+        $y = SessionControl::get();
+        self::assertSame( $x, $y->backend() );
+        self::assertSame( 12345, $y->lifetime() );
+    }
+
+
+    public function testSetForSessionControl() : void {
+        $x = new SessionControl();
+        SessionControl::set( $x );
+        self::assertSame( $x, SessionControl::get() );
+
 
     }
 
